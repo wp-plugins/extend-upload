d13601a74369f0488b8238cb87c7de9595d219e8
webord <webord@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-05 15:47:08 +0000

    Better Readme.txt and Version 0.0.2
    git-svn-id: https://plugins.svn.wordpress.org/extend-upload/trunk@527691 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extend-upload.php
+++ extend-upload.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Extend WordPress Upload
 Plugin URI: http://bordoni.me/wp/plugins/extend-upload
-Version: 0.0.1
+Version: 0.0.2
 Description: A WordPress plugin to Extend the default WordPress upload
 Author: Quatix
 Author URI: http://www.quatix.com.br
@@ -26,11 +26,38 @@ add_action( 'init', function() {
 	if ( preg_match( '/^https/', $pluginurl ) && !preg_match( '/^https/', get_bloginfo('url') ) )
 		$pluginurl = preg_replace( '/^https/', 'http', $pluginurl );
 
-	wp_register_script( 'extend-upload', $pluginurl . "/js/extend-upload.js", array( 'json2', 'jquery', 'thickbox', 'media-upload' ), '0.0.1', true );
-	wp_register_style( 'extend-upload', $pluginurl . "/css/extend-upload.css", array( 'thickbox' ), '0.0.1', 'screen' );
+	wp_register_script( 'extend-upload', $pluginurl . "/js/extend-upload.js", array( 'json2', 'jquery', 'thickbox', 'media-upload' ), '0.0.2', true );
+	wp_register_style( 'extend-upload', $pluginurl . "/css/extend-upload.css", array( 'thickbox' ), '0.0.2', 'screen' );
 }, 5 );
 
 add_action( 'image_send_to_editor', function( $html, $id, $caption, $title, $align, $url, $size, $alt ) {
 	$html = str_replace( 'src="' , 'data=\'' . json_encode( array( 'id'=>$id ) ) . '\' src="', $html );
 	return $html;
 }, 10, 8 );
+
+add_action( 'admin_head-media-upload-popup', function() {
+	if( isset($_GET['button']) && !empty($_GET['button']) ) {
+	?>
+	<script type='text/javascript'>/* <![CDATA[ */
+	(function($) {
+		$(window).load(function () {
+			var extup = { 'selector': {}, '$': {} };
+			extup.selector.item = '#media-items .media-item';
+			extup.selector.button = '#media-items .media-item td.savesend input.button[id^="send"]';
+			extup.selector.strictbutton = 'td.savesend input.button[id^="send"]';
+			extup.$.button = $(extup.selector.button);
+			extup.$.button.val("<?php echo esc_attr($_GET['button']); ?>");
+			$(document).on({
+				'hover': function() {
+					var $this = $(this),
+						$button = $this.find( extup.selector.strictbutton ),
+						attr = $this.attr('id');
+					$button.val("<?php echo esc_attr($_GET['button']); ?>");
+				}
+			}, extup.selector.item );
+		});
+	})(jQuery);
+	/* ]]> */</script>
+	<?php
+	}
+}, 20 );
